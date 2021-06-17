db7c098701e4b1e555ae76c6fd406c161c77a007
ponomarevandrew <Caf-Lw9-TV5-pku>
2021-06-17 16:48:39 +0300

    script

---
+++ main.py
@@ -0,0 +1,30 @@
+import csv
+import xml.etree.ElementTree as ET
+
+
+def get_numbers_from_csv(csv_file_name):
+    with open(csv_file_name, 'r') as infile:
+        reader = csv.reader(infile)
+        next(reader)
+        return [int(row[0]) for row in reader]
+
+
+def change_numbers_in_xml(xml_file_name, numbers_list, xml_path):
+    tree = ET.parse(xml_file_name)
+    root = tree.getroot()
+    element = root.find(xml_path)
+    for num in numbers_list:
+        num_element = ET.SubElement(element, f'num id = \"{num}\"')
+    tree.write(xml_file_name)
+
+
+CSV_FILE_NAME = "nums.csv"
+XML_FILE_NAME = "nums.xml"
+
+## путь до списка с числами не считая корневого элемента
+XMl_PATH_TO_NUMBER_LIST_ELEMENT = "nums"
+
+
+if __name__ == '__main__':
+    number_list = get_numbers_from_csv(CSV_FILE_NAME)
+    change_numbers_in_xml(XML_FILE_NAME, number_list, 'nums')
